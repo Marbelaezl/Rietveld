7044cc6a7778b0cfe8471a1d7591e832cac6fc40
Marbelaezl <124514421+Marbelaezl@users.noreply.github.com>
2023-07-04 17:38:53 -0500

    Vectorización de picos e intensidades

--- refinamiento2.py
+++ refinamiento2.py
@@ -6,13 +6,14 @@ Created on Tue Nov 29 21:24:02 2022
 """
 import numpy as np
 import matplotlib as mpl
-mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 import Wyckoff
 import tkinter as tk
 from tkinter import filedialog
 import tablas
 import time
+from numpy.linalg import norm
+mpl.use('TkAgg')
 
 indmax=8
 minang=10
@@ -34,16 +35,14 @@ ga=120*np.pi/180
 la=1.54060
 delta=0.0001
 cel1=np.array([
-    [22,0,0,0,0],
-    [22,1/3,2/3,0.50,0]
+    [22,0,0,0,0,1],
+    [22,1/3,2/3,0.50,0,1]
     ])
 ks1=[k,a,b,c,al,be,ga]
 # Ninguna de las estructuras del TiO2 estaba dando los resultados correctos
 # debido a un error en el cálculo de cvec. Buscar una nueva estructura e intentar de nuevo
-
-
-print()
 ks2=[100,4.593,4.593,2.9590,90*np.pi/180,90*np.pi/180,90*np.pi/180]
+ks2=[100,100,300,40,90*np.pi/180,90*np.pi/180,90*np.pi/180]
 #La estructura del rutilo depende de un parámetro interno, que era el que aparecía
 #como "x" en las posiciones de Wyckoff.
 #El rutilo (ahora simulado correctamente) no parece poder explicar el pico súbito
@@ -57,6 +56,11 @@ cel=np.array([
 cel2=Wyckoff.Wyckoff(136, cel)
 ks3=[100,3.7850 ,3.7850 ,9.520,90*np.pi/180,90*np.pi/180,90*np.pi/180]
 cel3=Wyckoff.Wyckoff(141, np.array([[22,0,0-1/4,0-3/8,0,1],[8,0,0-1/4,0.20806-3/8,0,1]]))
+testvecs=np.array([
+    [0.33898305,0,0],
+    [0,0.33898305,0],
+    [0,0,0.2177226214]
+    ])
 #print(cel3)
 #Predicciones correctas: Ti(alpha), diamante, NaCl, TiO2
 #Resultados = open("Resultados.dat",mode="w")
@@ -157,119 +161,152 @@ def peso(datos):
 
 # Funciones requeridas para el patrón:
     
-def disf(vecs,h,k,l):
+def disf(vecs,hkl):
     "Retorna la distancia interplanar del plano con índices h,k,l. vecs es la matriz de vectores EN ESPACIO RECÍPROCO"
+    h=hkl[0]
+    k=hkl[1]
+    l=hkl[2]
     ac=vecs[0]
     bc=vecs[1]
     cc=vecs[2]
-    return 1/(sum((h*ac+k*bc+l*cc)**2)**(1/2))
+    try:
+        return 1/(sum((h*ac+k*bc+l*cc)**2)**(1/2))
+    except:
+        return[-1]
 
 
-def angf(vecs,h,k,l):
-    if (la/(2*disf(vecs,h,k,l)))<1:
-        theta=np.arcsin(la/(2*disf(vecs,h,k,l)))
+def angf(vecs,hkl):
+    h=hkl[0]
+    k=hkl[1]
+    l=hkl[2]
+    if (la/(2*disf(vecs,[h,k,l])))<1:
+        theta=np.arcsin(la/(2*disf(vecs,[h,k,l])))
         return ([360*theta/np.pi,h,k,l,1])
 #Si el ángulo no existe (la codnición de Bragg da sin(theta)>1, la función devuelve [-1], negativo para que nunca sea igual a minang)
-    else:
-        return [-1]
+    return [-1]
     
-def picosf(vecs,indmax):
-    res1=[]
-    for l in range (-indmax,indmax):
-        if l%10==1:
-            print (l)
-        for k in range (-indmax,indmax):
-            for h in range (-indmax,indmax):
-                if [h,k,l]!=[0,0,0]:
-#Esta condición controla los ángulos máximos y mínimos del patrón predicho
-                    prov=angf(vecs,h,k,l) #Se guarda para no tener que calcularlo 3 veces
-                    if prov[0]<=maxang and prov[0]>=minang:
-                        res1.append(prov)                                                                             
-    res1=np.array(res1)
-    #print(res1)
-    return res1
+def vecdisf(vecs,hkl):
+    "disf vectorizada para tomar un array hkl más grande (bidimensional)"
+    return (1/np.linalg.norm(hkl@vecs,axis=1))
+
+def vecangf(vecs,hkl):
+    angs=(360*np.arcsin(la/(2*vecdisf(vecs,hkl))))/np.pi
+    prov=np.hstack([np.array([angs]).T,hkl,np.array([np.ones_like(angs)]).T])
+    indexes=np.where((prov[:,0]<maxang))
+    return prov[indexes]
+    
+
+
+
+def fastpicoshelp(ind,flag=True):
+    if flag:
+        prov=np.arange(0,(ind[0]+1)*(ind[1]+1)*(ind[2]+1),1,dtype='i4')
+        res=np.fix(np.vstack(np.array([prov/((ind[1]+1)*(ind[2]+1)),(prov/(ind[2]+1))%(ind[1]+1),prov%(ind[2]+1)]).T))
+        
+    else:
+        prov=np.arange(0,(2*ind[0]+1)*(2*ind[1]+1)*(2*ind[2]+1),1,dtype='i4')
+        res=np.floor(np.vstack(np.array([prov/((2*ind[1]+1)*(2*ind[2]+1)),(prov/(2*ind[2]+1))%(2*ind[1]+1),prov%(2*ind[2]+1)]).T))
+        res[:]=res[:]-ind
+    res=np.delete(res, (np.where(np.all(res==0,axis=-1))),axis=0)
+    return res
+
+
+
+def fastpicosf(vecs,indmax):
+    delta=1e-5
+    res=np.array([[0.0,0.0,0.0,0.0,1]])
+    for i in fastpicoshelp(indmax):
+        prov=angf(vecs,i)
+        ii=np.searchsorted(res[:,0],prov[0],side='right')
+        if prov[0]!=-1:
+            if np.abs(prov[0]-res[ii-1,0])>delta:
+                res=np.insert(res,ii,prov,axis=0)
+            else:
+                res[ii-1,-1]+=1
+    minii=np.searchsorted(res[:,0],minang,side='left')
+    maxii=np.searchsorted(res[:,0],maxang,side='right')
+    res=res[minii:maxii]
+    res[:,-1]=0
+    for i in fastpicoshelp(indmax,flag=False):
+        prov=angf(vecs,i)
+        prov[0]+=(delta/100)
+        ii=np.searchsorted(res[:,0],prov[0],side='right')
+        if np.abs(prov[0]-res[ii-1,0])<delta:
+            res[ii-1,-1]+=1
+    return (res)
+
+def vecpicosf(vecs,indmax):
+    logd=6 #logaritmo de delta
+    delta=10**-logd
+    prov=vecangf(vecs,fastpicoshelp(indmax))
+    prov=prov[prov[:, 0].argsort()]
+    prov = np.delete(prov, np.argwhere(np.ediff1d(prov[:,0].round(logd)) < delta/10) + 1,axis=0)
+    prov2=vecangf(vecs,fastpicoshelp(indmax,flag=False))
+    minii=np.searchsorted(prov[:,0],minang,side='left')
+    maxii=np.searchsorted(prov[:,0],maxang,side='right')
+    prov=prov[minii:maxii]
+    prov2=prov2[prov2[:, 0].argsort()]
+    minii=np.searchsorted(prov2[:,0],minang,side='left')
+    maxii=np.searchsorted(prov2[:,0],maxang,side='right')
+    prov2=prov2[minii:maxii]
+
+    print(prov)
+    
+    unique, counts = np.unique(np.round(prov2[:,0],logd), return_counts=True)
+    print(unique)
+    prov[:,-1]+=counts-1
+    return prov
 
 def longpicosf(vecs,indmax):
+    delta=1e-5
     res1=[]
     flag=True
-    for l in range (0,indmax):
-        if l%10==1:
-            print (l)
-        for k in range (0,indmax):
-            for h in range (0,indmax):
+    for l in np.arange(0,indmax[2]+1):
+        if l%20==1:
+            print ("índice actual:" +str(l-1))
+        for k in np.arange(0,indmax[1]+1):
+            for h in np.arange(0,indmax[0]+1):
                 if [h,k,l]!=[0,0,0]:
 #Esta condición controla los ángulos máximos y mínimos del patrón predicho
-                    prov=angf(vecs,h,k,l) #Se guarda para no tener que calcularlo 3 veces
+                    prov=angf(vecs,[h,k,l]) #Se guarda para no tener que calcularlo 3 veces
                     if prov[0]<=maxang and prov[0]>=minang:
                         for i in res1:
-                            if np.abs(i[0]-prov[0])<0.00001:
+                            if np.abs(i[0]-prov[0])<delta:
                                 flag=False
                         if flag:
                             res1.append(prov)
                         flag=True    
     #Factor de multiplicidad, no está funcionando. La implementación comentada funciona pero es lenta                                                                       
+    cont=0
     res1=np.array(res1)
     res1[:,-1]=0
     res1=res1[res1[:, 0].argsort()]
-    for l in range (-indmax,indmax):
-        if l%10==1:
-            print (l)
-        for k in range (-indmax,indmax):
-            for h in range (-indmax,indmax):
-                if [h,k,l]!=[0,0,0]:
-                    prov=angf(vecs,h,k,l)
-                    # if prov[0]<=maxang and prov[0]>=minang:
-                    #     for i in res1:
-                    #         if np.abs(i[0]-prov[0])<0.001:
-                    #             i[-1]+=1
-                    #             break
-                    
-                    prov[0]-=0.0001
-                    if prov[0]<=maxang and prov[0]>=minang:
-                        index=np.searchsorted(res1[:,0], prov[0])
-                        if index==1:
-                            print(str(h)+str(k)+str(l))
-                        res1[index,-1]+=1
-    print(res1)
+    for l in np.arange(-indmax[2],indmax[2]+1):
+        for k in np.arange(-indmax[1],indmax[1]+1):
+            for h in np.arange(-indmax[0],indmax[0]+1):
+                cont+=1
+                prov=angf(vecs,[h,k,l])
+                # if prov[0]<=maxang and prov[0]>=minang:
+                #     for i in res1:
+                #         if np.abs(i[0]-prov[0])<0.001:
+                #             i[-1]+=1
+                #             break
+                
+                prov[0]+=(delta/100)
+                if prov[0]<=maxang and prov[0]>=minang:
+                    index=np.searchsorted(res1[:,0], prov[0],side='right')
+                    res1[index-1,-1]+=1
+    print("Picos considerados por longpicosf:")
+    print(cont)
     return res1
 
-
-def multipf(picos1):
-    cond=True
-    i=0
-    j=0
-    while i<len(picos1):
-        while j<len(picos1):
-            if abs(picos1[i,0]-picos1[j,0])<10*delta and i!=j:
-                if picos1[i,1]>=0 and picos1[i,2]>=0 and picos1[i,3]>=0:
-                    picos1[i,4]=picos1[i,4]+picos1[j,4]
-                    picos1=np.delete(picos1,j,axis=0)
-                    cond=False
-                else:
-                    picos1[j,4]=picos1[i,4]+picos1[j,4]
-                    picos1=np.delete(picos1,i,axis=0)
-                    j=0
-                    cond=False
-
-            if cond:
-                j=j+1
-            cond=True
-        i=i+1
-        j=0
-    i=0
-    while i<len(picos1):
-        if (picos1[i,1]<0 or picos1[i,2]<0 or picos1[i,3]<0):
-            picos1=np.delete(picos1,i,axis=0)
-            i=i-2
-        i=i+1
-    return picos1 
 
 Cro_Mann=tablas.Cro_Mann
 def structf(multip,celda):
     "Halla el factor F_hkl de los planos"
     def atom_strucf(n,par,ion=0):
         "Devuelve el factor de estructura del átomo con número n según el parámetro par = sin(theta)/lambda"
-        "Solo considera la parte real. Se omiten efectos de dispersión anómala"
+        "Solo considera la parte real; se omiten efectos de dispersión anómala"
         return (Cro_Mann[ion,n,0]*np.e**(-Cro_Mann[ion,n,4]*(par**2))+
               Cro_Mann[ion,n,1]*np.e**(-Cro_Mann[ion,n,5]*(par**2))+
               Cro_Mann[ion,n,2]*np.e**(-Cro_Mann[ion,n,6]*(par**2))+
@@ -314,7 +351,18 @@ def intensidadesf(multip,struct):
             multip[:,4]=multip[:,4]/max(multip[:,4])
     multip[:,4]=multip[:,4]/max(multip[:,4])
     return multip
-  
+
+def fastintensidadesf(multip,struct):
+    #como la entrada a[i,0] es 2theta, se multiplica por pi/360 en lugar de pi/180
+    ang=multip[:,0]*np.pi/360
+    #Factor de Lorentz - Geometría Bragg-Brentano 
+    lo= (1+ (np.cos(2*ang)**2))/(np.sin(2*ang)*np.sin(ang))
+    multip[:,4]=np.abs(struct)**2 *lo*multip[:,4]
+    multip[:,4]=multip[:,4]/max(multip[:,4])
+    return multip
+    
+    
+
 def ord1f(intens):
     "Ordena los picos según el ángulo 2theta"
     cambios=0
@@ -363,33 +411,36 @@ def MinInfoPat(ctes=ks1,celda=cel1,ForceIndMax=9999):
     Lsin= 2*np.sin(maxang)/(la)
     #print(Lsin)
     #print(sum(ac**2)**(1/2))
-    indMax= min(int(max([Lsin/(sum(ac**2)**(1/2)),Lsin/(sum(bc**2)**(1/2)),Lsin/(sum(cc**2)**(1/2))]))+1,ForceIndMax) 
+    indMax= np.ceil([
+        min([Lsin/(sum(ac**2)**(1/2))+1,ForceIndMax]),
+        min([Lsin/(sum(bc**2)**(1/2))+1,ForceIndMax]),
+        min([Lsin/(sum(cc**2)**(1/2))+1,ForceIndMax])
+        ])
     print("indmax =" + str(indMax))
-    if indMax > 12:
-        multip=longpicosf(Vecs,indMax)
-        print("Factor de multiplicidad encontrado. Picos obtenidos ("+str(len(multip))+"):")
-    else:
-        picos= picosf(Vecs,indMax)
-        print("picos calculados correctamente. Picos encontrados:" + str(len(picos)))
-        multip=multipf(picos)
-        print("Factor de multiplicidad encontrado. Picos obtenidos ("+str(len(multip))+"):")
+    #multip=fastpicosf(Vecs,indMax)
+    multip=vecpicosf(Vecs, indMax)
+     #   print("Factor de multiplicidad encontrado. Picos obtenidos ("+str(len(multip))+"):")
+     #   print("picos calculados correctamente. Picos encontrados:" + str(len(picos)))
+    print("Factor de multiplicidad encontrado. Picos obtenidos ("+str(len(multip))+"):")
+    print(multip)
     end1=time.time()
     print("Tiempo de cálculo: " + str(end1-start))
-    
-   
-    end2=time.time()
-    print("Tiempo de cálculo: " + str(end2-end1))
-    
     struct=structf(multip,celda)
-    print("Factores de estructura encontrados:")
-    end3=time.time()
-    print("Tiempo de cálculo: " + str(end3-end2))
+    end2=time.time()
+    print("Tiempo de cálculo structf: " + str(end2-end1))
     
-    intensidades=intensidadesf(multip,struct)
-    print("Intensidades sin ordenar:")
+    #print("Factores de estructura encontrados:")
+    #end3=time.time()
+    #print("Tiempo de cálculo: " + str(end3-end2))
+    
+    intensidades=fastintensidadesf(multip,struct)
+    end3=time.time()
+    print("Tiempo de cálculo intensidadesf: " + str(end3-end2))
+    
+    #print("Intensidades sin ordenar:")
 
-    end4=time.time()
-    print("Tiempo de cálculo: " + str(end4-end3))
+    #end4=time.time()
+    #print("Tiempo de cálculo: " + str(end4-end3))
     
     patron=ord1f(intensidades)
     
@@ -397,7 +448,7 @@ def MinInfoPat(ctes=ks1,celda=cel1,ForceIndMax=9999):
     print("Tiempo de cálculo: " + str(end-start))
     return patron
 #patron=MinInfoPat()
-
+MinInfoPat(ks2)
     
 U=0.00
 V=0.00
